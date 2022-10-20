7b8d7c7a6b5073dbb4e2b3304ac60117b0a478e7
rodrigorunner <rodrigoregisrun@gmail.com>
2022-10-20 19:10:46 -0300

    CRUD done

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
+const Campoground = require('./models/campground')
+const methodOverride = require('method-override')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const express = require('express')
+const morgan = require('morgan')
 const path = require('path')
 const ejs = require('ejs')
 const app = express()
-const Campoground = require('./models/campground')
 
 connectDB()
 
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'))
+app.use(methodOverride('_method'))
+app.use('/', require('./routes/campgroundRoutes'))
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/campground', async (req, res) => {
-    const camp = new Campoground({title: 'My backyard', description: 'Is is my first one'})
-    await camp.save()
-    res.send(camp)
-})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000!')

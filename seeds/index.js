c1e3a4e8e7e8ad6afac0713656a3dc4c8c427dfc
rodrigorunner <rodrigoregisrun@gmail.com>
2022-10-21 14:57:04 -0300

    Bootstrap added

--- seeds/index.js
+++ seeds/index.js
@@ -13,13 +13,18 @@ const seedDB = async () => {
    await Campground.deleteMany({})
    for(let i = 0; i < 50; i++) {
       const random = Math.floor(Math.random() * 1000) 
+      const price = Math.floor(Math.random() + 30) + 10
       const camp = new Campground({
          location: `${cities[random].city}, ${cities[random].state}`,
-         title: `${sample(descriptors)} ${sample(places)}`
+         title: `${sample(descriptors)} ${sample(places)}`,
+         image: 'https://source.unsplash.com/collection/483251',
+         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, iure officiis! Fugiat quod unde ullam! In quis sit quasi hic perferendis? Reprehenderit voluptas veniam, animi adipisci similique molestias ullam earum.',
+         price
       })
       camp.save()
    }
 }
-seedDB().then(() => {
-   mongoose.connection.close()
-})
+seedDB()
+// .then(() => {
+//    mongoose.connection.close()
+// })
